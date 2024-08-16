9265643200cb3c938349ff6045af0413529ffc4c
ahmet <ahmtyln.06@gmail.com>
2024-08-16 23:45:11 +0200

    context api

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,12 +1,15 @@
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import apiRequest from "../../lib/apiRequest"
+import { useContext, useState } from "react";
+import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
 
   const[error, setError] = useState("")
   const[isLoading, setIsLoading] = useState(false)
+
+  const {updateUser} = useContext(AuthContext);
 
 
   const navigate = useNavigate();
@@ -25,7 +28,7 @@ function Login() {
       username,
       password,
     })
-    localStorage.setItem("user", JSON.stringify(res.data));
+    updateUser(res.data)
     navigate("/");
    } catch (err) {
     console.log(err)
